d81ed8e98ee0c7acb485984f21bfcc98179b9568
Hamza Mahmood <hmahmoo3@jh.edu>
2023-07-03 21:38:18 -0400

    Add in files related to the GUI app development.

---
+++ main_app.py
@@ -0,0 +1,44 @@
+import sys
+import typing
+from PyQt5.uic import loadUi
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication, QWidget
+
+class MainWindow(QDialog):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        loadUi("main_window/main_window.ui", self)
+        self.pushButton.clicked.connect(self.gotoBoard)
+    
+    def gotoBoard(self):
+        board=Board()
+        widget.addWidget(board)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+class Board(QDialog):
+    def __init__(self):
+        super(Board, self).__init__()
+        loadUi("board/board.ui", self)
+        self.pushButton_2.clicked.connect(self.gotoMenu)
+    
+    def gotoMenu(self):
+        main_app=Board()
+        widget.addWidget(main_app)
+        widget.setCurrentIndex(widget.currentIndex()-1)
+
+# main
+app = QApplication(sys.argv)
+widget=QtWidgets.QStackedWidget()
+main_app=MainWindow()
+board=Board()
+widget.setWindowTitle("TrivialCompute")
+widget.addWidget(main_app)
+widget.addWidget(board)
+widget.setFixedHeight(900)
+widget.setFixedWidth(880)
+widget.show()
+
+try:
+    sys.exit(app.exec_())
+except:
+    print("Exiting")
